2c940696e916e8aceca480f2378afa5c715ff722
SamidhaManjrekar <smanjrekar135@gmail.com>
2025-10-04 17:51:02 +0530

    menu items repository

---
+++ src/main/java/com/example/restaurant_system/repository/MenuItemRepository.java
@@ -0,0 +1,10 @@
+package com.example.restaurant_system.repository;
+
+import com.example.restaurant_system.entity.MenuItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
+
+}
